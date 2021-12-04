953ef8a7b5fac5f93e8bf5b92f117c33521e0448
Luiz Jardim <CTW00767@criticaltechworks.com>
2021-12-04 09:35:20 +0000

    refactored 1_1 to a fp style

--- src/1_1/main.rs
+++ src/1_1/main.rs
@@ -1,18 +1,37 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("/home/sousajo/etudes/adventofcode/input")
+    let contents = fs::read_to_string("input")
         .expect("Something went wrong reading the file");
 
-    let mut splitted = contents.split('\n');
-    let mut aux : i32 =  i32::MAX;
-    let mut cnt : i32 = 0;
-    for s in splitted {
-        let s_as_int : i32 = s.parse().unwrap();
-        if s_as_int > aux {
-            cnt += 1;
-        }
-        aux = s_as_int;
+    println!["{}", solve(&contents)];
+}
+
+fn solve(input: &str) -> usize {
+    input
+        .split('\n')
+        .map(
+            |x| x.parse::<i32>().unwrap()
+        )
+        .scan(
+            i32::MAX,
+            |previous, current| {
+                let increased = current > *previous;
+                *previous = current;
+                Some(increased)
+            }
+        )
+        .filter(|increased| *increased)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn example() {
+        assert_eq!(
+            solve("199\n200\n208\n210\n200\n207\n240\n269\n260\n263"),
+            7
+        );
     }
-    println!("{}", cnt);
 }
